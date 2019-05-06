d6fd4b905b2799df83891a8367a218bed8da1536
Jack Baron <jackmbaron@gmail.com>
2019-05-06 09:45:47 +0100

    tests for utils

--- test/randomFromArray.ts
+++ test/randomFromArray.ts
@@ -1,6 +1,22 @@
 import test from 'ava'
 import { randomFromArray } from '../src/utils'
 
+const testArray = ['a', 'b']
+
 test('is a function', t => {
   return t.is(typeof randomFromArray, 'function')
+})
+
+test('picks a random element from an array', t => {
+  for (let i = 0; i < 20; i++) {
+    const rand = randomFromArray(testArray)
+    t.assert(testArray.includes(rand))
+  }
+})
+
+test('does not select the previous element again', t => {
+  for (let i = 0; i < 20; i++) {
+    const rand = randomFromArray(testArray, 'a')
+    t.is(rand, 'b')
+  }
 })
